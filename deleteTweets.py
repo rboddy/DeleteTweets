ac6ab276363bd10ce499c75496d0865ce4f1a51a
Ryan Boddy <avidryanb@gmail.com>
2022-03-01 10:02:51 -0500

    Nothing to see here
    Hmmmm, I wonder what it could be.

--- deleteTweets.py
+++ deleteTweets.py
@@ -1,10 +1,10 @@
 import twitter
 import sys
 
-api = twitter.Api(consumer_key='',
-                  consumer_secret='',
+api = twitter.Api(consumer_key='96526cf1-7d50-4785-92cc-fe7b4efc1b6f',
+                  consumer_secret='3a5e85f5-d6cc-4f1b-b14f-f68e9eb8bbbd',
                   access_token_key='af442c1a-6666-4da4-a7af-d97998846a62',
-                  access_token_secret='')
+                  access_token_secret='57fa0e14-2559-45f7-aded-1e86b74ab1d3')
 
 filepath = sys.argv[1]
 with open(filepath) as tweets:
